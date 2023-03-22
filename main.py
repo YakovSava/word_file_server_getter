f2d338361af9d753d05b550d9a0de71855cec519
Jacob Savel'ev <g6h6m238929@gmail.com>
2023-03-22 18:21:24 +0500

    Version 0.2
    Beta version

--- main.py
+++ main.py
@@ -1,18 +1,48 @@
-import asyncio
-
+from os import listdir
 from aiohttp.web import Response, run_app, Application, RouteTableDef, Request
+from plugins.binderr import Binder
 
 routes = RouteTableDef()
 app = Application()
+binder = Binder()
 
 
 @routes.get('/')
 async def main_handler(request: Request):
-	return Response(body='Server is start!')
+    data = await binder.get_html('main.html')
+    data['body'] = (data['body']
+                    .replace('(No files)',
+                             ''.join(
+                                 f'<a href="/word?{filename}">{filename}</a><br>\n'
+                                 for filename in (listdir('files'))
+                             )
+                             )
+                    )
+    return Response(
+        body=data['body'],
+        content_type=data['content_type'],
+        status=data['status']
+    )
 
-@routes.get('/file')
-async def file_get_handler(request:Request):
-	pass
+
+@routes.get('/styles')
+async def css_handler(request: request):
+    data = await binder.get_css(str(request.url).split('?')[1])
+    return Response(
+        body=data['body'],
+        content_type=data['content_type'],
+        status=data['status']
+    )
+
+
+@routes.get('/word')
+async def file_get_handler(request: Request):
+    data = await binder.get_wordfile(str(request.url).split('?')[1])
+    return Response(
+        body=data['body'],
+        content_type=data['content_type'],
+        status=data['status']
+    )
 
 if __name__ == '__main__':
-	run_app(app, host='192.168.100.8', port='80')
+    run_app(app, host='192.168.100.4', port='80')
--- plugins/binder.py
+++ plugins/binder.py
@@ -1,15 +1,58 @@
-import asyncio
-
 from os.path import join
 from urllib.parse import unquote
 from aiofiles import open as aiopen
 
+
 class Binder:
 
-	def __init__(self, path:str='files'):
-		self._path = path
+    def __init__(self, path: str='files', html_path: str='html'):
+        self._path = path
+        self._html = html_path
 
-	async def _get_wordfile(self, filename:str) -> dict:
-		try:
-			async with aiopen(join(self._path, filename)) as file:
-				file_data = await file.read()
+    async def get_wordfile(self, filename: str) -> dict:
+        try:
+            async with aiopen(join(self._path, unquote(filename)), 'rb') as file:
+                data = {
+                    'content_type': 'application/msword',
+                    'body': await file.read(),
+                    'status': 200
+                }
+        except:
+            data = {
+                'content_type': 'text/plain',
+                'body': '',
+                'status': 404
+            }
+        return data
+
+    async def get_css(self, style_name: str) -> dict:
+        try:
+            async with aiopen(join(self._html, unquote(filename)), 'r', encoding='utf-8') as file:
+                data = {
+                    'content_type': 'text/css',
+                    'body': await file.read(),
+                    'status': 200
+                }
+        except:
+            data = {
+                'content_type': 'text/plain',
+                'body': '',
+                'status': 404
+            }
+        return data
+
+    async def get_html(self, html_name: str) -> dict:
+        try:
+            async with aiopen(join(self._html, unquote(filename)), 'r', encoding='utf-8') as file:
+                data = {
+                    'content_type': 'text/html',
+                    'body': await file.read(),
+                    'status': 200
+                }
+        except:
+            data = {
+                'content_type': 'text/plain',
+                'body': '',
+                'status': 404
+            }
+        return data
